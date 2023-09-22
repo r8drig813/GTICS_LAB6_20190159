319ec230aac690db747e3a1555a18573bec27941
Rodrigo Tinoco <a20190159@pucp.edu.pe>
2023-09-21 20:27:56 -0500

    primer commit

---
+++ lab6_20190159/src/test/java/com/example/lab6_20190159/Lab620190159ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20190159;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620190159ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
